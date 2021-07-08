1bf4b2fa8737a62d57ebda055cb5f704477cc8d8
Rewsage <rewsage@gmail.com>
2021-07-08 09:39:53 +0800

    Change the appearance of the header on small screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -29,9 +29,9 @@ const Header = () => {
         console.log(location.pathname)
         return true
         return [Urls.feed.slugRoot, Urls.timetable.slugEdit,
-        Urls.panel.slugRoot, Urls.root,
-        Urls.post.slugRoot, Urls.post.slugCreate,
-        Urls.user.slugMe, Urls.user.slugRoot, Urls.user.slugProfile].includes(location.pathname)
+            Urls.panel.slugRoot, Urls.root,
+            Urls.post.slugRoot, Urls.post.slugCreate,
+            Urls.user.slugMe, Urls.user.slugRoot, Urls.user.slugProfile].includes(location.pathname)
     }
 
     const [collapseIsOpen, collapseChange] = useState(false);
@@ -43,12 +43,51 @@ const Header = () => {
     return (
         <div>
             {Rendered() ? <div className="navbar__main">
-                <MDBNavbar className="mb-3" color="teal lighten-2" dark expand="md">
-                    <MDBNavbarBrand>
+                <MDBNavbar className="mb-3 justify-content-between" color="teal lighten-2" dark expand="lg">
+                    <MDBNavbarToggler onClick={toggleCollapse} />
+
+                    <MDBNavbarBrand className="navbar__brand mr-0 mr-lg-3">
                         <strong className="white-text">10-tka</strong>
                     </MDBNavbarBrand>
-                    <MDBNavbarToggler onClick={toggleCollapse}/>
-                    <MDBCollapse id="navbarCollapse3" isOpen={collapseIsOpen} navbar>
+
+                    <div className="order-lg-last">
+                        <MDBNavbarNav>
+                            <div className="d-flex align-items-center justify-content-end flex-nowrap">
+                                <MDBNavItem className="px-3 px-lg-0">
+                                    <img className="navbar__avatar"
+                                         onClick={() => { history.push(Urls.user.slugMe) }}
+                                         src={localStorage.getItem("user_role") === "admin" ? Admin : Avatar}
+                                         alt="oops"/>
+                                </MDBNavItem>
+                                <MDBNavItem
+                                    className="container-fluid flex-column justify-content-center text-center d-none d-lg-block">
+                                <span
+                                    className="main__title">{localStorage.getItem("user_role") === "admin" ? "Admin" : "Вася Пупкин"}</span>
+                                    <div className="text-black-50">
+                                        <Timer size={{sm: true}} onZero={() => {
+                                            Logout(history);
+                                        }}/>
+                                    </div>
+                                </MDBNavItem>
+                                <MDBNavItem className="d-none d-lg-block">
+                                    <MDBDropdown>
+                                        <MDBDropdownToggle className="dropdown-toggle navbar__dropdown">
+                                        </MDBDropdownToggle>
+                                        <MDBDropdownMenu right basic>
+                                            <MDBDropdownItem onClick={() => {
+                                                history.push(Urls.user.slugMe)
+                                            }}>Профиль</MDBDropdownItem>
+                                            <MDBDropdownItem onClick={() => {
+                                                Logout(history);
+                                            }}>Выйти</MDBDropdownItem>
+                                        </MDBDropdownMenu>
+                                    </MDBDropdown>
+                                </MDBNavItem>
+                            </div>
+                        </MDBNavbarNav>
+                    </div>
+
+                    <MDBCollapse id="navbarCollapse" isOpen={collapseIsOpen} navbar>
                         <MDBNavbarNav left className="col-lg-8">
                             <MDBNavItem active>
                                 <MDBNavLink className="navbar__main_link" to={Urls.feed.slugRoot}>
@@ -72,7 +111,7 @@ const Header = () => {
                                     </MDBNavLink>
                                 </MDBNavItem> : null}
 
-                            <MDBNavItem active className="d-md-none">
+                            <MDBNavItem active className="d-lg-none">
                                 <button type="button"
                                         className="link-logout"
                                         onClick={() => {
@@ -81,40 +120,6 @@ const Header = () => {
                                     <div className="navbar__main_strong">Выход</div>
                                 </button>
                             </MDBNavItem>
-                        </MDBNavbarNav>
-                        <MDBNavbarNav right className=" d-none d-md-block">
-                            <div
-                                className="d-flex align-items-center justify-content-end flex-nowrap">
-                                <MDBNavItem>
-                                    <img className="navbar__avatar"
-                                         src={localStorage.getItem("user_role") === "admin" ? Admin : Avatar}
-                                         alt="oops"/>
-                                </MDBNavItem>
-                                <MDBNavItem
-                                    className="container-fluid flex-column justify-content-center text-center">
-                                    <span
-                                        className="main__title">{localStorage.getItem("user_role") === "admin" ? "Admin" : "Вася Пупкин"}</span>
-                                    <div className="text-black-50">
-                                        <Timer size={{sm: true}} onZero={() => {
-                                            Logout(history);
-                                        }}/>
-                                    </div>
-                                </MDBNavItem>
-                                <MDBNavItem>
-                                    <MDBDropdown>
-                                        <MDBDropdownToggle className="dropdown-toggle navbar__dropdown">
-                                        </MDBDropdownToggle>
-                                        <MDBDropdownMenu right basic>
-                                            <MDBDropdownItem onClick={() => {
-                                                history.push(Urls.user.slugMe)
-                                            }}>Профиль</MDBDropdownItem>
-                                            <MDBDropdownItem onClick={() => {
-                                                Logout(history);
-                                            }}>Выйти</MDBDropdownItem>
-                                        </MDBDropdownMenu>
-                                    </MDBDropdown>
-                                </MDBNavItem>
-                            </div>
                         </MDBNavbarNav>
                     </MDBCollapse>
                 </MDBNavbar>
